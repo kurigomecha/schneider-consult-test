684f61cb1b2a81569095b4352ea6163b65cf43a8
kurigomecha <rey22test@gmail.com>
2022-11-18 18:30:08 +0400

    Add `getNextMonday` function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-/*
+function getNextMonday(date = new Date()) {
+  const dateCopy = new Date(date.getTime());
 
-get current date;
-calculate date of next Monday based on current date;
-update `sale-end-date` in HTML to date of next Moday;
+  const nextMonday = new Date(
+    dateCopy.setDate(
+      dateCopy.getDate() + ((7 - dateCopy.getDay() + 1) % 7 || 7), // if today is Monday, default to 7
+    ),
+  );
 
-*/
+  return nextMonday;
+}
+
+
+// change `sale-end-date` to next Monday in RU locale
+
+document.querySelector(`.sale-end-date`).textContent =
+getNextMonday().toLocaleDateString(`ru-RU`);
